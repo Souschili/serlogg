9cd07a79c0165d3303fe46943ae5319d515e89c3
Orkhan <aliyevorhan82@gmail.com>
2024-01-26 02:01:52 +0400

    finish configure serilog with code

--- serlogg/Program.cs
+++ serlogg/Program.cs
@@ -1,4 +1,5 @@
 using Serilog;
+using Serilog.Enricher.ClientInfo;
 using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,12 +12,13 @@ Log.Logger = new LoggerConfiguration()
     //will cause all events from Microsoft, Microsoft.AspNetCore,
     //Microsoft.AspNetCore.Hosting, etc., to be logged at the Warning level
     // the same with system
-    .MinimumLevel.Override("Microsoft",LogEventLevel.Error)  
-    .MinimumLevel.Override("Microsoft.AspNetCore",LogEventLevel.Warning)  
-    .MinimumLevel.Override("System",LogEventLevel.Error)
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+    .MinimumLevel.Override("System", LogEventLevel.Error)
     .WriteTo.Console()
-    .WriteTo.File("Logs/app-log-.txt",rollingInterval:RollingInterval.Day,
-    rollOnFileSizeLimit:true,fileSizeLimitBytes: 524288000)
+    .WriteTo.File("Logs/app-log-.txt", rollingInterval: RollingInterval.Day,
+    rollOnFileSizeLimit: true, fileSizeLimitBytes: 524288000)
+
     .CreateLogger();
 
 // Add services to the container.
@@ -35,7 +37,19 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseSerilogRequestLogging();
+app.UseSerilogRequestLogging(
+    options =>
+    {
+        options.MessageTemplate =
+            "{RemoteIpAddress} {RequestScheme} {RequestHost} {RequestMethod} {RequestPath} responded {StatusCode} in {Elapsed:0.0000} ms";
+        options.EnrichDiagnosticContext = (
+            diagnosticContext,httpContext) =>
+        {
+            diagnosticContext.Set("RequestHost", httpContext.Request.Host.Value);
+            diagnosticContext.Set("RequestScheme", httpContext.Request.Scheme);
+            diagnosticContext.Set("RemoteIpAddress", httpContext.Connection.RemoteIpAddress);
+        };
+    });
 
 app.UseHttpsRedirection();
 
